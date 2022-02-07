772e974d1ced247b00cbc609f5f5c76b819dcae5
Daria Ablovatskaia <daryana_chizhova@list.ru>
2022-02-07 12:39:35 +0700

    Add main page and catalog items.

--- src/components/elements/Item.js
+++ src/components/elements/Item.js
@@ -1,7 +1,12 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 const Item = (props) => {
   const { title, images, sku, manufacturer, color, material, season, reason, sizes } = props.item;
+  const [selectedSize, setSelectedSize] = useState();
+
+  const onSelectSize = (size) => {
+    setSelectedSize(size);
+  };
 
   return (
       <section className="catalog-item">
@@ -17,40 +22,45 @@ const Item = (props) => {
 
             <tr>
               <td>Артикул</td>
-              <td>{sku}</td>
+              <td>{sku || ''}</td>
             </tr>
 
             <tr>
             <td>Производитель</td>
-            <td>{manufacturer}</td>
+            <td>{manufacturer || ''}</td>
             </tr>
 
             <tr>
               <td>Цвет</td>
-              <td>{color}</td>
+              <td>{color || ''}</td>
             </tr>
 
             <tr>
               <td>Материалы</td>
-              <td>{material}</td>
+              <td>{material || ''}</td>
             </tr>
 
             <tr>
               <td>Сезон</td>
-              <td>{season}</td>
+              <td>{season || ''}</td>
             </tr>
 
             <tr>
               <td>Повод</td>
-              <td>{reason}</td>
+              <td>{reason || ''}</td>
             </tr>
             </tbody>
       </table>
 
         <div className="text-center">
           <p className="sizes">Размеры в наличии:
-            <span className="catalog-item-size selected">{sizes.size}</span>
-            <span className="catalog-item-size">{sizes.size}</span></p>
+            {
+              sizes
+                .filter((item) => item.avalible === true)
+                .map((item) => <span className="catalog-item-size" onClick={() => onSelectSize(item.size)}>{item.size}</span>)
+            }
+            <span className={(item) => item.size === selectedSize ? "catalog-item-size selected" : "catalog-item-size"}></span>
+          </p>
           <p>Количество: <span className="btn-group btn-group-sm pl-2">
             <button className="btn btn-secondary">-</button>
             <span className="btn btn-outline-primary">1</span>
@@ -58,7 +68,7 @@ const Item = (props) => {
           </span>
           </p>
         </div>
-        <button className="btn btn-danger btn-block btn-lg">В корзину</button>
+          { selectedSize ? <button className="btn btn-danger btn-block btn-lg">В корзину</button> : null }
       </div>
       </section>
   );
